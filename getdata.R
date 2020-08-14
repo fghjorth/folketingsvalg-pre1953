250012a5ada3e8ac80af8cccd204493858e68e24
Frederik Hjorth <frederikhjorth@gmail.com>
2020-08-14 10:46:03 +0200

    ocr function

---
+++ getdata.R
@@ -0,0 +1,49 @@
+setwd("~/Documents/GitHub/folketingsvalg-pre1953")
+
+library(tesseract)
+library(magick)
+library(stringr)
+library(tidyverse)
+
+lettervec <- 
+
+getyrres <- function(year,lettervec){
+  
+  imgx <- image_read(paste0("images/",1939,".jpg"))
+  
+  ocrx <- image_ocr(imgx)
+  
+  ocrxtib1 <- ocrx %>% 
+    str_split(.,"\n") %>% 
+    pluck(1) %>% 
+    as_tibble() %>% 
+    rename(original=value)
+  
+  eligible <- ocrxtib1$original[str_which(ocrxtib1$original,"stemmeberettigede")] %>% 
+    str_remove_all(.,"\\.") %>% 
+    str_extract(.,"[0-9]{7,8}") %>% 
+    as.numeric()
+  
+  validvotes <- ocrxtib1$original[str_which(ocrxtib1$original,"gyldige")] %>% 
+    str_remove_all(.,"\\.") %>% 
+    str_extract(.,"[0-9]{7,8}") %>% 
+    as.numeric()
+  
+  ocrxtib2 <- ocrxtib1 %>% 
+    mutate(partyvotes=str_extract(original,"[0-9]{1,3},[0-9]{3}"),
+           partyvotes=as.numeric(str_remove(partyvotes,","))) %>% 
+    filter(!is.na(partyvotes)) %>% 
+    mutate(letter=lettervec)
+  
+  ocrxtib_wide <- ocrxtib %>% 
+    dplyr::select(partyvotes,letter) %>% 
+    pivot_wider(names_from = letter,values_from=partyvotes,names_prefix="party_") %>% 
+    mutate(total_valid=validvotes,
+           electorate=eligible,
+           year=year) %>% 
+    dplyr::select(year,everything())
+  
+  return(ocrxtib_wide)
+}
+
+yr1939 <- getyrres(1939,c("s","rv","k","v","rfb","bp","dkp","n","ds","sp","ns" ))
